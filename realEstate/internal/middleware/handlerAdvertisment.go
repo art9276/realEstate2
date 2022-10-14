b10bf26ef248b02f26d4ee22a94068d9abbce79a
art9276 <art927715@gmail.com>
2022-10-14 16:16:57 +0300

    Add files via upload
    Create filtering advertisment

--- realEstate/internal/middleware/handlerAdvertisment.go
+++ realEstate/internal/middleware/handlerAdvertisment.go
@@ -7,6 +7,7 @@ import (
 	"realEstate/internal/db"
 	"realEstate/internal/models"
 	"strconv"
+	"strings"
 )
 
 func GetAdvertisment(c *gin.Context) {
@@ -38,16 +39,109 @@ func GetAdvertisment(c *gin.Context) {
 }
 
 func GetAllAdvertisment(c *gin.Context) {
-
-	rows, err := db.InitDB().Query(`SELECT "IdAdvertisment", 
+	query:=`SELECT "IdAdvertisment", 
        "TypeAdvertisment", "Price", "TotalArea",  
        "YearOfContribution", "Address", "Description", 
        "NumberOfRooms","IsCommercial"
-	FROM public."Advertisment"`)
+	FROM public."Advertisment" where 1=1`
+	//order by $1 asc Limit $2 OFFSET $3
+	TypeAdvertisment:= c.Query("TypeAdvertisment")
+	if TypeAdvertisment!="" {
+		query =query + ` AND "TypeAdvertisment"='`+ TypeAdvertisment + `'`
+	}
+	PriceL:=c.Query("PriceL")
+	if PriceL!="" {
+		strconv.Atoi(PriceL)
+		query =query + ` AND "Price" >='`+ PriceL + `'`
+	}
+	PriceU:=c.Query("PriceU")
+	if PriceU!="" {
+		strconv.Atoi(PriceU)
+		query =query + ` AND "Price" <='`+ PriceU + `'`
+	}
+	Price:=c.Query("Price")
+	if Price!="" {
+		strconv.Atoi(Price)
+		query =query + ` AND "Price" ='`+ Price + `'`
+	}
+	TotalAreaL:= c.Query("TotalAreaL")
+	if TotalAreaL!="" {
+		strconv.Atoi(TotalAreaL)
+		query =query + ` AND "TotalArea" >='`+ TotalAreaL + `'`
+	}
+	TotalAreaU:= c.Query("TotalAreaU")
+	if TotalAreaU !="" {
+		strconv.Atoi(TotalAreaU)
+		query =query + ` AND "TotalArea" <='`+ TotalAreaU + `'`
+	}
+	TotalArea:= c.Query("TotalArea")
+	if TotalArea !="" {
+		strconv.Atoi(TotalArea)
+		query =query + ` AND "TotalArea" ='`+ TotalArea + `'`
+	}
+	YearOfContribution:=c.Query("YearOfContribution")
+	if YearOfContribution!="" {
+		strconv.Atoi(YearOfContribution)
+		query =query + ` AND "YearOfContribution" ='`+ YearOfContribution + `'`
+	}
+	YearOfContributionL:=c.Query("YearOfContributionL")
+	if YearOfContributionL!="" {
+		strconv.Atoi(YearOfContributionL)
+		query =query + ` AND "YearOfContribution" >='`+ YearOfContributionL + `'`
+	}
+	YearOfContributionU:=c.Query("YearOfContributionU")
+	if YearOfContributionU!="" {
+		strconv.Atoi(YearOfContributionU)
+		query =query + ` AND "YearOfContribution" <='`+ YearOfContributionU + `'`
+	}
+	Address :=c.Query("Address")
+	if Address !="" {
+		query =query + ` AND "Address" ='`+ Address + `'`
+	}
+	NumberOfRooms:=c.Query("NumberOfRooms")
+	if NumberOfRooms !="" {
+		strconv.Atoi(NumberOfRooms)
+		query =query + ` AND "NumberOfRooms" ='`+ NumberOfRooms + `'`
+	}
+	NumberOfRoomsL:=c.Query("NumberOfRoomsL")
+	if NumberOfRooms !="" {
+		strconv.Atoi(NumberOfRoomsL)
+		query =query + ` AND "NumberOfRooms" >='`+ NumberOfRoomsL + `'`
+	}
+	NumberOfRoomsU:=c.Query("NumberOfRoomsU")
+	if NumberOfRoomsU !="" {
+		strconv.Atoi(NumberOfRoomsU)
+		query =query + ` AND "NumberOfRooms" <='`+ NumberOfRoomsU + `'`
+	}
+	IsCommercial:=c.Query("IsCommercial")
+	if IsCommercial !="" {
+		strconv.Atoi(IsCommercial)
+		query =query + ` AND "IsCommercial"='`+ IsCommercial + `'`
+	}
+	Start:= c.Query("Start")
+	if Start==""{Start="0"
+	}
+	Start2, _ :=strconv.Atoi(Start)
+	println(Start2)
+	Limit:= c.Query("Limit")
+	if Limit==""{Limit="10"
+	}
+	Limit2, _ :=strconv.Atoi(Limit)
+	println(Limit2)
+	SortBy:=c.Query("SortBy")
+	if SortBy==""{SortBy="YearOfContribution"
+	}
+	SortOrder:=c.Query("SortOrder")
+	strings.ToUpper(SortOrder)
+	if SortOrder==""{query+=` order by $1 ASC Limit $2 OFFSET $3`}
+	if SortOrder=="asc"{query=query + ` order by $1 ASC Limit $2 OFFSET $3`}
+	if SortOrder=="desc" {query=query + ` order by $1 DESC Limit $2 OFFSET $3`}
+	rows, err := db.InitDB().Query(query,SortBy,Limit2,Start2) //SortOrder
 	if err != nil {
-		c.JSON(http.StatusOK, gin.H{
+		c.JSON(http.StatusInternalServerError, gin.H{
 			"message": "Query not Scanned",
 		})
+		return
 	}
 	defer rows.Close()
 	var advertisments []models.Advertisment
@@ -61,8 +155,9 @@ func GetAllAdvertisment(c *gin.Context) {
 		// Exit if we get an error
 		if err2 != nil {
 			c.JSON(http.StatusInternalServerError, gin.H{
-				"message": "Users not scanned",
+				"message": "Adwertisment not scanned",
 			})
+			return
 		}
 		advertisments = append(advertisments, advertisment)
 	}
@@ -77,6 +172,7 @@ func CreateAdvertisment(c *gin.Context) {
 		c.JSON(http.StatusInternalServerError, gin.H{
 			"message": "JSON non scanned",
 		})
+		return
 	}
 	sqlStatement := `INSERT INTO public."Advertisment"(
 	"TypeAdvertisment", "Price", "TotalArea", 
@@ -90,11 +186,13 @@ func CreateAdvertisment(c *gin.Context) {
 		c.JSON(http.StatusInternalServerError, gin.H{
 			"message": "Failed to saved advertisment",
 		})
+		return
 	} else {
 		fmt.Println(res)
 		c.JSON(http.StatusCreated, gin.H{
 			"Advertisment created": a,
 		})
+		return
 	}
 }
 
@@ -104,6 +202,7 @@ func UpdateAdvertisment(c *gin.Context) {
 		c.JSON(http.StatusInternalServerError, gin.H{
 			"message": "JSON non scanned",
 		})
+		return
 	}
 	sqlStatement := `UPDATE public."Advertisment"
 	SET  "TypeAdvertisment"=$1, "Price"=$2, 
@@ -117,12 +216,13 @@ func UpdateAdvertisment(c *gin.Context) {
 		c.JSON(http.StatusInternalServerError, gin.H{
 			"message": "Failed to updated advertisment",
 		})
+		return
 	} else {
 		fmt.Println(res)
-
 		c.JSON(http.StatusOK, gin.H{
 			"Advertisment updated": a,
 		})
+		return
 	}
 }
 
@@ -133,6 +233,7 @@ func DeleteAdvertisment(c *gin.Context) {
 		c.JSON(http.StatusInternalServerError, gin.H{
 			"message": "Could not convert string to int",
 		})
+		return
 	}
 	sqlStatement := `DELETE FROM public."Advertisment"
 	WHERE "IdAdvertisment"=$1`
@@ -141,9 +242,11 @@ func DeleteAdvertisment(c *gin.Context) {
 		c.JSON(http.StatusBadRequest, gin.H{
 			"message": "Advertisment not found",
 		})
+		return
 	} else {
 		fmt.Println(res)
 
 		c.JSON(http.StatusOK, "Advertisment deleted")
 	}
+	return
 }
